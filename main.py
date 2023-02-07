31ad411c8c1c4e8b90b74615b026625e45e2b54b
mattroski <mattroski88@gmail.com>
2023-02-06 21:11:11 -0600

    Can find single post, Status Codes

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
+from random import randrange
 
 app = FastAPI()
 
@@ -11,18 +12,39 @@ class Post(BaseModel):
     published: bool = True #optional field, user doesnt need to provide, defaults to true
     rating: Optional[int] = None #another optional, must be an int if created, 
 
+my_posts = [{"title":"title post 1", "content":"content post 1", "id":1},
+{"title":"title post 2", "content":"content post 2", "id":2}]
+
+def find_post(id):
+    id = int(id)
+    for p in my_posts:
+        if p["id"] == id:
+            return p
+
+
 @app.get("/")
 async def root():
     return {"message": "welcome!"}
 
 @app.get("/posts")
 async def get_posts():
-    return {"data":"this is a post"}
+    return {"data":my_posts}#fastapi will serialize the array, turning into json
 
-@app.post("/createposts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)#if creating sucessful, return 201
 def create_posts(post: Post):#extract all the fields from the body, convent into a python dictionary, store inside a variable named payload
-    print(post)
-    print(post.dict()) #converts our pydanic model into a dictionary
-    return {"data": post}
+    #(post.dict()) #converts our pydanic model into a dictionary
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0,1000000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
 #post.title -> top beaches in florida
 #title str, content str
+
+@app.get("/posts/{id}") #id is a path parameter, path parameters are always returned as a string. 
+def get_post(id: int, response: Response): #validates and converts id into an int
+    post = find_post(id)
+    if not post:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,
+        detail=f"post with id: {id} was not found")
+      
+    return {"post_detail": f"Here is post {post}"}
